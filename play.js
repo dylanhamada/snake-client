f16fc6c380463fd31d029f2918adf27a0ce6b498
dylanhamada <dylan.hamada@gmail.com>
2023-10-25 15:47:13 -0400

    Add functionality to print received server data to client

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",// IP address here,
+    port: "50541"// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  // print incoming data to player
+  conn.on("data", (data) => {
+    console.log(`Data received: ${data}`);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
